21e7f5617ec594a68eb48429cd7050ecd726bd55
Adrian Rusznica <adrian.rusznica@leica-geosystems.com>
2024-05-03 22:37:56 +0000

    Clean

--- test/my-powerlevel10k/test.sh
+++ test/my-powerlevel10k/test.sh
@@ -1,35 +1,5 @@
 #!/bin/bash
 
-# This test file will be executed against an auto-generated devcontainer.json that
-# includes the 'color' Feature with no options.
-#
-# For more information, see: https://github.com/devcontainers/cli/blob/main/docs/features/test.md
-#
-# Eg:
-# {
-#    "image": "<..some-base-image...>",
-#    "features": {
-#      "color": {}
-#    },
-#    "remoteUser": "root"
-# }
-#
-# Thus, the value of all options will fall back to the default value in the
-# Feature's 'devcontainer-feature.json'.
-# For the 'color' feature, that means the default favorite color is 'red'.
-#
-# These scripts are run as 'root' by default. Although that can be changed
-# with the '--remote-user' flag.
-# 
-# This test can be run with the following command:
-#
-#    devcontainer features test    \ 
-#               --features color   \
-#               --remote-user root \
-#               --skip-scenarios   \
-#               --base-image mcr.microsoft.com/devcontainers/base:ubuntu \
-#               /path/to/this/repo
-
 set -e
 
 # Optional: Import test library bundled with the devcontainer CLI
