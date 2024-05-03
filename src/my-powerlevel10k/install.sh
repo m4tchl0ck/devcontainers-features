fb03e6d521213c252db97fd817125d515599155b
Adrian Rusznica <adrian.rusznica@leica-geosystems.com>
2024-05-03 23:39:50 +0000

    Fix theme path

--- src/my-powerlevel10k/install.sh
+++ src/my-powerlevel10k/install.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 set -e
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $_CONTAINER_USER_HOME/powerlevel10k
-chown -R $_CONTAINER_USER:$_CONTAINER_USER $_CONTAINER_USER_HOME/powerlevel10k
-cp $_CONTAINER_USER_HOME/powerlevel10k/powerlevel10k.zsh-theme $_CONTAINER_USER_HOME/.oh-my-zsh/custom/themes/powerlevel10k/powerlevel10k.zsh-theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $_REMOTE_USER_HOME/powerlevel10k
+chown -R $_REMOTE_USER:$_REMOTE_USER $_REMOTE_USER_HOME/powerlevel10k
+ls $_REMOTE_USER_HOME
+cp $_REMOTE_USER_HOME/powerlevel10k/powerlevel10k.zsh-theme $_REMOTE_USER_HOME/.oh-my-zsh/custom/themes/powerlevel10k.zsh-theme
 
 echo "my-powerlevel10k installed"
-# echo 'source $_CONTAINER_USER_HOME/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+# echo 'source $_REMOTE_USER_HOME/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
