5f0df417ce979dc087b07078871333f0f77153ac
m4tchl0ck <m4tchl0ck@outlook.com>
2024-05-14 23:08:22 +0200

    Copy theme

--- src/my-powerlevel10k/install.sh
+++ src/my-powerlevel10k/install.sh
@@ -2,4 +2,6 @@
 set -e
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $_CONTAINER_USER_HOME/powerlevel10k
-echo 'source $_CONTAINER_USER_HOME/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+chown -R $_CONTAINER_USER:$_CONTAINER_USER $_CONTAINER_USER_HOME/powerlevel10k
+cp $_CONTAINER_USER_HOME/powerlevel10k/powerlevel10k.zsh-theme $_CONTAINER_USER_HOME/.oh-my-zsh/custom/themes/powerlevel10k/powerlevel10k.zsh-theme
+# echo 'source $_CONTAINER_USER_HOME/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
