b0471afddaab24f0f5dbeb446dd787e0c68e1fbc
m4tchl0ck <m4tchl0ck@outlook.com>
2024-05-14 23:09:12 +0200

    Check .zshrc after installation

--- src/my-powerlevel10k/install.sh
+++ src/my-powerlevel10k/install.sh
@@ -15,4 +15,4 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $_REMOTE_USER_H
 cp $_REMOTE_USER_HOME/powerlevel10k/powerlevel10k.zsh-theme $_REMOTE_USER_HOME/.oh-my-zsh/custom/themes/powerlevel10k.zsh-theme
 
 echo "my-powerlevel10k installed"
-echo 'source $_REMOTE_USER_HOME/powerlevel10k/powerlevel10k.zsh-theme' >>$_REMOTE_USER_HOME/.zshrc
+echo 'source '$_REMOTE_USER_HOME'/powerlevel10k/powerlevel10k.zsh-theme' >>$_REMOTE_USER_HOME/.zshrc
