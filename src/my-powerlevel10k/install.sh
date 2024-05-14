b1b6cd892cad0e2108c3498387bd8b7a886e7833
m4tchl0ck <m4tchl0ck@outlook.com>
2024-05-14 23:08:53 +0200

    Clean up powerlevel10 installation script

--- src/my-powerlevel10k/install.sh
+++ src/my-powerlevel10k/install.sh
@@ -2,9 +2,7 @@
 set -e
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $_REMOTE_USER_HOME/powerlevel10k
-chown -R $_REMOTE_USER:$_REMOTE_USER $_REMOTE_USER_HOME/powerlevel10k
-ls $_REMOTE_USER_HOME
 cp $_REMOTE_USER_HOME/powerlevel10k/powerlevel10k.zsh-theme $_REMOTE_USER_HOME/.oh-my-zsh/custom/themes/powerlevel10k.zsh-theme
 
 echo "my-powerlevel10k installed"
-# echo 'source $_REMOTE_USER_HOME/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+echo 'source $_REMOTE_USER_HOME/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
