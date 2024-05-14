41f42732ea05292692ce347bc1b31a7f863a4774
m4tchl0ck <m4tchl0ck@outlook.com>
2024-05-14 23:07:46 +0200

    Add my-powerlevel10k feature

---
+++ src/my-powerlevel10k/install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+
+# The 'install.sh' entrypoint script is always executed as the root user.
+#
+# These following environment variables are passed in by the dev container CLI.
+# These may be useful in instances where the context of the final 
+# remoteUser or containerUser is useful.
+# For more details, see https://containers.dev/implementors/features#user-env-var
+echo "The effective dev container remoteUser is '$_REMOTE_USER'"
+echo "The effective dev container remoteUser's home directory is '$_REMOTE_USER_HOME'"
+
+echo "The effective dev container containerUser is '$_CONTAINER_USER'"
+echo "The effective dev container containerUser's home directory is '$_CONTAINER_USER_HOME'"
+
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $_CONTAINER_USER_HOME/powerlevel10k
+echo 'source $_CONTAINER_USER_HOME/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
